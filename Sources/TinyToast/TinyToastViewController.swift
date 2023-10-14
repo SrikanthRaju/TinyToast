6d6d7bcde0dd9a4ce825d945b3b6c3ea646bb9e6
Srikanth <srikanth.pandeti@gmail.com>
2023-10-14 18:32:06 +0530

    cooler update

--- Sources/TinyToast/TinyToastViewController.swift
+++ Sources/TinyToast/TinyToastViewController.swift
@@ -73,7 +73,7 @@ final class TinyToastViewController: UIViewController {
         toastView?.backgroundColor = backgroundColor
         toastView?.layer.borderWidth = 0.5
         toastView?.layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
-        toastView?.layer.shadowColor = backgroundColor.cgColor
+        toastView?.layer.shadowColor = backgroundColor.withAlphaComponent(0.4).cgColor
         toastView?.layer.shadowOpacity = 0.4
         toastView?.layer.shadowRadius = 6
         
